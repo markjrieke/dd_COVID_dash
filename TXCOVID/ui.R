444e39c4b6bf83fe8003d334397b7a16f58d0002
markjrieke <markjrieke@gmail.com>
2021-03-15 22:22:18 -0500

    update from sidebar to bootstrap layout

--- TXCOVID/ui.R
+++ TXCOVID/ui.R
@@ -3,6 +3,7 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(shinythemes)
 
 # setup ----
 f_pop <- as_tibble(read.csv("data/county_names.csv"))
@@ -13,45 +14,78 @@ rm(f_pop)
 
 # ui ----
 shinyUI(
-    fluidPage(
-        
-        # App title
-        headerPanel("Texas COVID Dashboard"),
-        
-        sidebarPanel(
+    bootstrapPage(
+        navbarPage(
+            h3("Texas COVID-19 Dashboard"),
+            theme = shinytheme("sandstone"),
+            collapsible = TRUE,
             
-            # Information to display:
-            radioButtons("count_var", "Compare:",
-                         list("Cases" = "cases",
-                              "Deaths" = "deaths")),
-            # Plot Type:
-            radioButtons("count_type", "Time Period:",
-                         list("14-day average" = "avg",
-                              "Cumulative" = "cum")),
+            tabPanel(
+                h4("State Information"),
+                
+                tabsetPanel(
+                    tabPanel(
+                        h4("Map"),
+                        h4("Leaflet plot coming soon")
+                    ),
+                    
+                    tabPanel(
+                        h4("State Data"),
+                        h4("Plotly plot coming soon")
+                    ),
+                    
+                    tabPanel(
+                        h4("Log-Log Plot"),
+                        h4("Log-Log Plot coming soon")
+                    )
+                )
+            ),
             
-            br(),
+            tabPanel(
+                h4("County Comparison"),
+                
+                sidebarPanel(
+                    
+                    # Information to display
+                    radioButtons("count_var", "Compare:",
+                                 list("Cases" = "cases",
+                                      "Deaths" = "deaths")),
+                    
+                    # Plot type:
+                    radioButtons("count_type", "Time Period:",
+                                 list("14-day average" = "avg",
+                                      "Cumulative" = "cum")),
+                    
+                    br(),
+                    
+                    # Total or per 100,000
+                    radioButtons("count_scale", NULL,
+                                 list("Per 100,000" = "norm",
+                                      "Total" = "abs")),
+                    
+                    # Select county
+                    selectInput("county1", "Compare up to 3 counties:",
+                                l_counties,
+                                selected = l_counties[215]),
+                    
+                    selectInput("county2", NULL,
+                                l_counties,
+                                selected = l_counties[57]),
+                    
+                    selectInput("county3", NULL,
+                                l_counties,
+                                selected = l_counties[101])
+                ),
+                
+                mainPanel(
+                    plotlyOutput("lineplot")
+                )
+            ),
             
-            # Total or per 100,000
-            radioButtons("count_scale", NULL,
-                         list("Per 100,000" = "norm",
-                              "Total" = "abs")),
-            
-            # Select County
-            selectInput("county1", "Compare up to 3 counties:",
-                        l_counties,
-                        selected = l_counties[215]),
-            
-            selectInput("county2", NULL,
-                        l_counties,
-                        selected = l_counties[57]),
-            
-            selectInput("county3", NULL,
-                        l_counties,
-                        selected = l_counties[101])
-        ),
-        mainPanel(
-            plotlyOutput("lineplot")
+            tabPanel(
+                h4("About"),
+                h4("About section coming soon")
+            )
         )
     )
 )
-
